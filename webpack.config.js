79229647e3e2cd6c24ed2f16a927f96ce14383c3
Gregory Olive <gregoryolive@outlook.com>
2022-03-20 13:52:43 +0900

    Run build

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      //favicon: './src/favicon.png',
+      favicon: './src/favicon.png',
     }),
   ],
   output: {
